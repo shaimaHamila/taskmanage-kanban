ebdc59ef3d957f831fdb7ffa97357fa97c00f677
shaimaHamila <hamilachaima18@gmail.com>
2025-05-07 11:13:15 +0300

    Add login component and routs

--- app/Livewire/Component/Auth/Login.php
+++ app/Livewire/Component/Auth/Login.php
@@ -3,9 +3,42 @@
 namespace App\Livewire\Component\Auth;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Component
 {
+    public $email;
+    public $password;
+
+    public function login()
+    {
+        $this->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $user = User::where('email', $this->email)->first();
+        if (!$user) {
+            $this->addError('email', 'This email is not registered.');
+            session()->flash('LoginError', 'Email not found.');
+            return;
+        }
+
+        if (!password_verify($this->password, $user->password)) {
+            $this->addError('password', 'The password is incorrect.');
+            session()->flash('LoginError', 'Incorrect password.');
+            return;
+        }
+
+        Auth::login($user);
+        return redirect()->route('tasks');
+    }
+    public function mount()
+    {
+        if (Auth::check()) {
+            return redirect()->route('tasks');
+        }
+    }
     public function render()
     {
         return view('livewire.component.auth.login');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Component\Auth\Login;
+use App\Livewire\Component\Task\ListTasks;
+use App\Livewire\Component\User\ListUsers;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('*', Login::class)->name('login');
+Route::get('/todos', ListTasks::class)->middleware('auth')->name('tasks');
+Route::get('/users', ListUsers::class)->middleware('auth')->name('users');

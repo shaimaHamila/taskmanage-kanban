b52411522c08f960a32fe0a18f9b2dbae80258b0
shaimaHamila <hamilachaima18@gmail.com>
2025-05-07 11:16:16 +0300

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Livewire\Component\Auth\Login;
 use App\Livewire\Component\Task\ListTasks;
 use App\Livewire\Component\User\ListUsers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('*', Login::class)->name('login');
+
+Route::get('/', Login::class)->name('login');
 Route::get('/todos', ListTasks::class)->middleware('auth')->name('tasks');
 Route::get('/users', ListUsers::class)->middleware('auth')->name('users');
